ff6312898fee2d9b9bd4768288300d0188a0e704
HanyiYe0 <hanyiye2@gmail.com>
2025-03-10 22:54:25 -0400

    new streams generated when finished animating

--- main.py
+++ main.py
@@ -1,44 +1,28 @@
-import random
 import pygame
-import threading
-from text import Text
+from stream import Stream
 
 #  CONSTANTS
 COLUMNS = 108
-DISTANCE_Y_BETWEEN_LETTERS = 11
-DISTANCE_X_BETWEEN_LETTERS = 10
 FADE_SPEED = 40
-PINK = (255, 192, 203)
 
 #  Initializers
 pygame.init()
-pygame.font.init()
 screen = pygame.display.set_mode((1080, 720))
 running = True
 clock = pygame.time.Clock()
 
-#  TEXTS AND FONTS
-love_font = pygame.font.SysFont('Roboto', 20)
-LOVE_LETTERS = (love_font.render('L', False, PINK),
-                love_font.render('O', False, PINK),
-                love_font.render('V', False, PINK),
-                love_font.render('E', False, PINK),
-                love_font.render('I', False, PINK),
-                love_font.render('Y', False, PINK),
-             )
+
+streams = []
+max_priorities = []
 
 #  COLUMN CREATION SECTION
-letters = []
-max_priorities = []
 #  Multiple column creation
 for x in range(COLUMNS):
-    priority_variant = random.randint(0, 20)
-    #  Column creation
-    end = random.randint(10, 50)
-    max_priorities.append(end)
-    for y in range(end):
-        letter = random.choice(LOVE_LETTERS)
-        letters.append(Text(letter, x * DISTANCE_X_BETWEEN_LETTERS, y * DISTANCE_Y_BETWEEN_LETTERS, y + priority_variant))
+    s = Stream(x)
+    streams.append(s)
+    s.generate_stream()
+    max_priorities.append(s.max_priorities)
+
 
 #  MAIN PROGRAM SECTION
 while running:
@@ -48,12 +32,24 @@ while running:
     #  Background
     screen.fill("black")
 
-    #  Stream animation
-    for letter in letters:
-        #  Fade in, fade out animation
-        letter.fade(FADE_SPEED)
-        letter.text.set_alpha(letter.alpha)
-        screen.blit(letter.text, (letter.x, letter.y))
+    for i in range(len(streams)):
+        #  Checker for finished full stream animation
+        if max_priorities[i] <= 0:
+            #  Generate new stream and remove old
+            new_s = Stream(streams[i].x)
+            streams[i] = new_s
+            new_s.generate_stream()
+            max_priorities[i] = new_s.max_priorities
+
+        max_priorities[i] -= 1
+
+        #  Stream animation
+        for letter in streams[i].letters:
+            #  Fade in, fade out animation
+            letter.fade(FADE_SPEED)
+            letter.text.set_alpha(letter.alpha)
+            screen.blit(letter.text, (letter.x, letter.y))
+            
     pygame.display.flip()
     clock.tick(10)
 pygame.quit()
---
+++ stream.py
@@ -0,0 +1,41 @@
+import random
+
+import pygame
+from text import Text
+
+#  INITIALIZERS
+pygame.font.init()
+
+#  CONSTANTS
+DISTANCE_Y_BETWEEN_LETTERS = 11
+DISTANCE_X_BETWEEN_LETTERS = 10
+PINK = (255, 192, 203)
+#  TEXTS AND FONTS
+love_font = pygame.font.SysFont('Roboto', 20)
+LOVE_LETTERS = (love_font.render('L', False, PINK),
+                love_font.render('O', False, PINK),
+                love_font.render('V', False, PINK),
+                love_font.render('E', False, PINK),
+                love_font.render('I', False, PINK),
+                love_font.render('Y', False, PINK),
+             )
+
+class Stream:
+    letters: list = []
+    max_priorities: int
+    x: int
+    def __init__(self, x):
+        self.letters = []
+        self.x = x
+
+    def generate_stream(self):
+        priority_variant = random.randint(0, 30)
+        #  Column creation
+        end = random.randint(10, 50)
+        self.max_priorities = end + priority_variant
+        for y in range(end):
+            letter = random.choice(LOVE_LETTERS)
+            self.letters.append(Text(letter, self.x * DISTANCE_X_BETWEEN_LETTERS, y * DISTANCE_Y_BETWEEN_LETTERS, y + priority_variant))
+
+    def get_x(self) -> int:
+        return self.x
