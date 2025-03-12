87c25085e8a5ad63f78619c907c70a2582c7fe40
HanyiYe0 <hanyiye2@gmail.com>
2025-03-11 23:29:42 -0400

    clean coordinates function implemented

--- image_coordinate_generator.py
+++ image_coordinate_generator.py
@@ -1,11 +1,20 @@
 import tkinter
 
+def clean_coordinate(coords: list) -> None:
+    x_min = min([coords[i][0] for i in range(len(coords))])
+    y_min = min([coords[i][1] for i in range(len(coords))])
+    for i in range(len(coords)):
+        coords[i][0] -= x_min
+        coords[i][1] -= y_min
+
+
 def get_coordinates():
     output = []
     for i in range(len(checkbuttons)):
         for j in range(len(checkbuttons[i])):
             if variables[i][j].get():
-                output.append((i, j))
+                output.append([i, j])
+    clean_coordinate(output)
     txt.delete('1.0', "end")
     txt.insert("1.0", str(output))
 
